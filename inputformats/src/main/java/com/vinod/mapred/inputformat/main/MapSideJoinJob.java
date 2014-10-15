94d556bb5e18b486ad8ff953b0091668c2d7c06e
vinod <vinod@Vinod>
2014-10-15 23:33:26 +0530

    Emp,Sal map side join using multi input

--- inputformats/src/main/java/com/vinod/mapred/inputformat/main/MapSideJoinJob.java
+++ inputformats/src/main/java/com/vinod/mapred/inputformat/main/MapSideJoinJob.java
@@ -13,13 +13,17 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import com.vinod.mapred.inputformat.avro.util.LongPair;
-import com.vinod.mapred.inputformat.join1.SalJoinMapper;
 import com.vinod.mapred.inputformat.join1.EmpJoinReducer;
 import com.vinod.mapred.inputformat.join1.EmployeeJoinMapper;
 import com.vinod.mapred.inputformat.join1.KeyValueLongInputFormat;
-import com.vinod.mapred.inputformat.util.TextPair;
+import com.vinod.mapred.inputformat.join1.SalJoinMapper;
 
-public class MapSideJoinJob extends Configured implements Tool{
+/**
+ * 
+ * @author vinod
+ *Emp,Sal map side join using multiple inputs
+ */
+public class MapSideJoinJob extends Configured implements Tool {
 
 	private Configuration conf;
 
@@ -33,46 +37,53 @@ public class MapSideJoinJob extends Configured implements Tool{
 
 		this.conf = conf;
 	}
-	 public static class KeyPartitioner extends Partitioner<LongPair, Text> {
-		    @Override
-		    public int getPartition(/*[*/LongPair key/*]*/, Text value, int numPartitions) {
-		      return (/*[*/key.getFirst().hashCode()/*]*/ & Integer.MAX_VALUE) % numPartitions;
-		    }
-		  }
+
+	public static class KeyPartitioner extends Partitioner<LongPair, Text> {
+		@Override
+		public int getPartition(/* [ */LongPair key/* ] */, Text value,
+				int numPartitions) {
+			return (/* [ */key.getFirst().hashCode()/* ] */& Integer.MAX_VALUE)
+					% numPartitions;
+		}
+	}
+
 	@Override
 	public int run(String[] args) throws Exception {
-		   /* if (args.length != 3) {
-	      JobBuilder.printUsage(this, "<ncdc input> <station input> <output>");
-	      return -1;
-	    }*/
-	    
-	    Job job = new Job(getConf(), "Join weather records with station names");
-	    job.setJarByClass(getClass());
-	    
-	    Path emp = new Path("/home/vinod/git/inputformats/inputformats/src/main/resources/join/emp.txt");
-	    Path sal = new Path("/home/vinod/git/inputformats/inputformats/src/main/resources/join/sal.txt");
-	    Path outputPath = new Path("/home/vinod/work/output/empjoin");
-	    outputPath.getFileSystem(getConf()).delete(outputPath, true);
-	    
-	    MultipleInputs.addInputPath(job, emp,
-	        KeyValueLongInputFormat.class, EmployeeJoinMapper.class);
-	    MultipleInputs.addInputPath(job, sal,
-	    		KeyValueLongInputFormat.class, SalJoinMapper.class);
-	    FileOutputFormat.setOutputPath(job, outputPath);
-	    job.setGroupingComparatorClass(LongWritable.Comparator.class);
-	    job.setPartitionerClass(KeyPartitioner.class);
-	    job.setMapOutputKeyClass(LongPair.class);
-	    
-	    job.setReducerClass(EmpJoinReducer.class);
+		/*
+		 * if (args.length != 3) { JobBuilder.printUsage(this,
+		 * "<ncdc input> <station input> <output>"); return -1; }
+		 */
 
-	    job.setOutputKeyClass(Text.class);
-	    
-	    return job.waitForCompletion(true) ? 0 : 1;
-	  }
+		//
+		Job job = new Job(getConf(), "Join weather records with station names");
+		job.setJarByClass(getClass());
+
+		Path emp = new Path(
+				"/home/vinod/git/inputformats/inputformats/src/main/resources/join/emp.txt");
+		Path sal = new Path(
+				"/home/vinod/git/inputformats/inputformats/src/main/resources/join/sal.txt");
+		Path outputPath = new Path("/home/vinod/work/output/empjoin");
+		outputPath.getFileSystem(getConf()).delete(outputPath, true);
+
+		MultipleInputs.addInputPath(job, emp, KeyValueLongInputFormat.class,
+				EmployeeJoinMapper.class);
+		MultipleInputs.addInputPath(job, sal, KeyValueLongInputFormat.class,
+				SalJoinMapper.class);
+		FileOutputFormat.setOutputPath(job, outputPath);
+		job.setGroupingComparatorClass(LongWritable.Comparator.class);
+		job.setPartitionerClass(KeyPartitioner.class);
+		job.setMapOutputKeyClass(LongPair.class);
+
+		job.setReducerClass(EmpJoinReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		int result = ToolRunner.run(conf,new MapSideJoinJob(), args);
+		int result = ToolRunner.run(conf, new MapSideJoinJob(), args);
 		System.exit(result);
 	}
 }
